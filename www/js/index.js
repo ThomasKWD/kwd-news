b6e22c001f7af6cb8775a31f152aa694d5fe8da1
root <root@localhost>
2014-04-10 00:57:26 +0200

    isDevice

--- www/js/index.js
+++ www/js/index.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
     window.isDevice = false; 
     
     if(document.URL.indexOf("http://") == -1 
-        && document.URL.indexOf("https://") == 1)
+        && document.URL.indexOf("https://") == 1
         && document.URL.indexOf("/kwd-news/") == -1)
  {
         window.isDevice = true;
