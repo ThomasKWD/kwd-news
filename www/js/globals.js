fb7b50e62d71e73845ded361c66ab7b5c56fe5f7
Thomas Kuhne S4 <thomas@kuehne-webdienste.de>
2015-03-08 14:55:09 +0100

    placeholder +text

--- www/js/globals.js
+++ www/js/globals.js
@@ -56,7 +56,7 @@ function KwdApp()	 {
 	// private members:
 	
 	var remoteBase = "";
-	var myplaceholder = 'res/kwd-4-placeholder-gradient.png'; // TODO: weißes Bild mit kleinem "k" in der Mitte
+	var myplaceholder = 'res/kwd-4-placeholder-weiss.png'; // TODO: weißes Bild mit kleinem "k" in der Mitte
 	
 	// wird gespeichert (auch in localStorage) und an enthaltene Objekte weitergegeben:
 	var updatemode = 'auto'; 
