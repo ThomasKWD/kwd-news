7626b26d5868861167af2937f2adf90cf425f6e2
Thomas Kühne <Wencke@.(none)>
2015-11-08 16:10:59 +0100

    separate files structure (no js or css in html)

---
+++ www/js/version.js
@@ -0,0 +1,4 @@
+/* useful when main files already packed and version number is to be corrected
+ * this value will be read by the kwdTachoApp object
+ */
+kwdTachoVersion = 1.52; // is float val by purposes
