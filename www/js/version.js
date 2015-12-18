1ada111fbba1a33ce037f22c5fb71bc1a5a577cc
Thomas Kühne <thomas@kuehne-webseiten.de>
2015-12-18 17:23:20 +0100

    tacho removed

--- www/js/version.js
+++
@@ -1,7 +0,0 @@
-/* useful when main files already packed and
- * version number or other global settings are to be corrected
- * these values will be read by the kwdTachoApp object
- */
-kwdTachoVersion = 1.57; // is float val by purpose
-kwdProjectStage = 'basic'; // can left unset if default desired
-kwdDebugMode = false; 
